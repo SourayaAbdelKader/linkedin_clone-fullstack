0a971ad2ced5a456ad0c93e03348eedaddf24ab5
Sourayaak <sourayaabdelkader@gmail.com>
2022-10-16 20:17:39 +0300

    Backend: Companies: adding functions to get a company information

--- backend/controllers/companies.controller.js
+++ backend/controllers/companies.controller.js
@@ -5,3 +5,26 @@ const Company = require('../models/companies.model');
 const Follow = require('../models/follow.model');
 const Application = require('../models/applications.model');
 const Job = require('../models/jobs.model');
+
+const getAllCompanies = async (req, res)=>{
+    const companies = await Company.find();
+    res.json(companies);
+}
+
+const getCompanyById = async (req, res)=>{
+    const {id} = req.params;
+    const company = await Company.findOne({id});
+    res.json(company);
+}
+
+const getCompanyrByEmail = async (req, res)=>{
+    const {id} = req.params;
+    const company = await Company.findOne({email});
+    res.json(company);
+}
+
+module.exports = {
+    getAllCompanies,
+    getCompanyrByEmail,
+    getCompanyById
+}
